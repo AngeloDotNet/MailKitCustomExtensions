2ac050420dca6f2ac1c4b88c5119e7098e433337
Angelo Pirola <angelo9871@gmail.com>
2023-03-13 21:13:43 +0100

    Fix

--- src/MailKitCustomExtensions/Extentions/DependencyInjection.cs
+++ src/MailKitCustomExtensions/Extentions/DependencyInjection.cs
@@ -5,7 +5,6 @@ public static class DependencyInjection
     public static IServiceCollection AddEmailSenderService(this IServiceCollection services, IConfiguration configuration)
     {
         services
-            .AddTransient<IUtilService, UtilService>()
             .AddSingleton<IEmailSender, MailKitEmailSender>()
             .AddSingleton<IEmailClient, MailKitEmailSender>();
 
